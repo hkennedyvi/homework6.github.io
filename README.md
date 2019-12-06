eaaabe88da2b26e298de781174c81c6326dc71dc
Howard Kennedy <hkennedyvi@gmail.com>
2019-12-05 23:09:54 -0800

    UV index function fixed, cities search now displays to browser

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # homework6.github.io
+
+The function of this application is to provide a user with weather information so that they may plan their travels accordingly. 
+
+Any visitor will have the ability to find an up to the minute forecast for a destination of their choosing. Data provided includes temperature, humidity, wind speed, and a UV index along with a five day forecast following the date searched. 
+
+Previous searched cities will be stored on the user's browser for quick recall. Additonally, a search history will be created and stored in local storage.
+
+The application utilizes an html and css file, a javscript file, and weather data provided by the API OpenWeatherMap. 
+
--- script.js
+++ script.js
@@ -1,46 +1,14 @@
 var queryURLBase1 = "http://api.openweathermap.org/data/2.5/weather?q=";
 var queryURLBase2 = "http://api.openweathermap.org/data/2.5/forecast?q=";
-var cityName = "Portland";
+var queryURLBase3 = "http://api.openweathermap.org/data/2.5/uvi?lat=";
 var authKey = "6ea95deef040e0312c5d12a77e2056ce";
-var queryURL1 = queryURLBase1 + cityName + "&APPID=" + authKey;
-var queryURL2 = queryURLBase2 + cityName + "&APPID=" + authKey;
 var dateToday = moment().format('l');
 var date2 = moment().add(1, 'days').format('l');
 var date3 = moment().add(2, 'days').format('l');
 var date4 = moment().add(3, 'days').format('l');
 var date5 = moment().add(4, 'days').format('l');
-
-function giveForecast() {
-
-    $.ajax({
-        url: queryURL2,
-        method: "GET"
-    }).then(function (drivethru) {
-
-        var farenheit1 = ((drivethru.list[0].main.temp - 273.15) * 9 / 5 + 32).toFixed();
-        var farenheit2 = ((drivethru.list[8].main.temp - 273.15) * 9 / 5 + 32).toFixed();
-        var farenheit3 = ((drivethru.list[16].main.temp - 273.15) * 9 / 5 + 32).toFixed();
-        var farenheit4 = ((drivethru.list[24].main.temp - 273.15) * 9 / 5 + 32).toFixed();
-
-        console.log(drivethru);
-        console.log(drivethru.list[0].main.temp);
-        console.log(drivethru.list[8].main.temp);
-        console.log(drivethru.list[16].main.temp);
-        console.log(drivethru.list[24].main.temp);
-        console.log(drivethru.list[0].dt_txt);
-        $(".date2").text(date2);
-        $(".temp2").text(farenheit1);
-        $(".date3").text(date3);
-        $(".temp3").text(farenheit2);
-        $(".date4").text(date4);
-        $(".temp4").text(farenheit3);
-        $(".date5").text(date5);
-        $(".temp5").text(farenheit4);
-    })
-
-};
-
-
+var latitude;
+var longitude;
 
 $("#submitBtn").on("click", function () {
     searchTerm = $("#user-search").val().trim();
@@ -53,15 +21,21 @@ $("#submitBtn").on("click", function () {
     }).then(function (results) {
 
         var farenheit = ((results.main.temp - 273.15) * 9 / 5 + 32).toFixed();
+        var weatherIcon = $("<img>");
+        weatherIcon.attr("src", "http://openweathermap.org/img/w/" + results.weather[0].icon + ".png");
 
-        console.log(results);
         $(".city-main").text(results.name + " " + dateToday);
-        $(".temp-main").text("Temperature: " + farenheit + " degrees F");
+        $(".city-main").append(weatherIcon);
+        $(".temp-main").text("Temperature: " + farenheit + " ºF");
         $(".humid-main").text("Humidity: " + results.main.humidity + " %");
         $(".wind-main").text("Wind Speed: " + results.wind.speed + " MPH");
         $(".date1").text(dateToday);
-        $(".temp1").text(farenheit);
-        console.log(farenheit + "cooool");
+        $(".temp1").text(farenheit + " ºF");
+
+
+        latitude = results.coord.lat;
+        longitude = results.coord.lon;
+        giveUV(latitude, longitude);    
 
     })
 
@@ -69,20 +43,54 @@ $("#submitBtn").on("click", function () {
 
 });
 
+
+
+function giveForecast() {
+    searchTerm = $("#user-search").val().trim();
+
+    var newURL2 = queryURLBase2 + searchTerm + "&APPID=" + authKey;
+
+    $.ajax({
+        url: newURL2,
+        method: "GET"
+    }).then(function (drivethru) {
+
+        var farenheit1 = ((drivethru.list[0].main.temp - 273.15) * 9 / 5 + 32).toFixed();
+        var farenheit2 = ((drivethru.list[8].main.temp - 273.15) * 9 / 5 + 32).toFixed();
+        var farenheit3 = ((drivethru.list[16].main.temp - 273.15) * 9 / 5 + 32).toFixed();
+        var farenheit4 = ((drivethru.list[24].main.temp - 273.15) * 9 / 5 + 32).toFixed();
+
+        $(".date2").text(date2);
+        $(".temp2").text(farenheit1 + " ºF");
+        $(".date3").text(date3);
+        $(".temp3").text(farenheit2 + " ºF");
+        $(".date4").text(date4);
+        $(".temp4").text(farenheit3 + " ºF");
+        $(".date5").text(date5);
+        $(".temp5").text(farenheit4 + " ºF");
+    })
+
+};
+
+function giveUV(latitude, longitude) {
+
+    //latitude = "29";
+    //longitude = "-127.5";
+
+    var newURL3 = queryURLBase3 + latitude + "&lon=" + longitude + "&APPID=" + authKey;
+
+    $.ajax({
+        url: newURL3,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response.value);
+    })
+};
+
+//giveUV();
+
 var cityList = $("#city-list");
 var cityHistory = [];
-
-renderCities();
-
-function renderCities() {
-    for (var i = 0; i < cityHistory.length; i++) {
-        var city = cityHistory[i];
-
-        var li = document.createElement("li");
-        li.textContent = city;
-        cityList.appendChild(li);
-    }
-};
 
 $("#submitBtn").on("click", function (event) {
     event.preventDefault();
@@ -98,20 +106,21 @@ $("#submitBtn").on("click", function (event) {
 
     console.log(cityHistory);
 
-    localStorage.setItem('Previous City Searched', cityHistory);
+    //localStorage.setItem('Previous City Searched', cityHistory);
+    localStorage.setItem(cityHistory, cityHistory);
+    renderCities(newCity);
+
+
 });
 
-/*function runQuery() {
+function renderCities(newCity) {
+    //for (var i = 0; i < cityHistory.length; i++) {
+        //var city = cityHistory[i];
 
-    $.ajax({
-        url: queryURL1,
-        method: "GET"
-    }).then(function (drivethru) {
-        console.log(drivethru);
-        $(".city-main").text(drivethru.name + " " + dateToday);
-        $(".temp-main").text("Temperature: " + drivethru.main.temp);
-        $(".humid-main").text("Humidity: " + drivethru.main.humidity);
-        $(".wind-main").text("Wind Speed: " + drivethru.wind.speed);
-    })
-};*/
-
+        var li = $("<li>");
+        li.text(newCity);
+        $("#city-list").append(li);
+        console.log("CITIESRENDERED");
+    //
+    
+};
